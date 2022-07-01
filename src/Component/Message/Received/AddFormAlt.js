35945d945fbdb4fb749f6cc0a3f50d0d7517fb3b
syone <syone7@gmail.com>
2022-07-01 10:12:01 +0200

    fix js

--- src/Component/Message/Received/AddFormAlt.js
+++ src/Component/Message/Received/AddFormAlt.js
@@ -1,5 +1,5 @@
 (function() {
-	$('.account-checkbox').change(function(e) {
+	$('body').on('change', '.account-checkbox', function(e) {
 		$(this.form).find('.account-ok').toggle();
 		e.stopImmediatePropagation();
 	});
