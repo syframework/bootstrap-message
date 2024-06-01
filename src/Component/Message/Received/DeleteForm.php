ad90548276ebbb9c3d952f324cda600bdabc3af6
Syone <syone7@gmail.com>
2024-06-01 14:01:00 +0200

    fix: move translation calls into init()

--- src/Component/Message/Received/DeleteForm.php
+++ src/Component/Message/Received/DeleteForm.php
@@ -3,18 +3,27 @@ namespace Sy\Bootstrap\Component\Message\Received;
 
 class DeleteForm extends \Sy\Bootstrap\Component\Form\Crud\Delete {
 
+	/**
+	 * @var int
+	 */
+	private $messageId;
+
+	/**
+	 * @param int $messageId
+	 */
 	public function __construct($messageId) {
-		parent::__construct(
-			'messageReceived',
-			['id' => $messageId],
-			[
-				'button-attributes' => ['class' => 'btn-sm'],
-				'confirm' => 'Are you sure to delete this message?',
-				'flash-message' => 'Message deleted successfully',
-				'selector' => '#message-' . $this->post('id', $messageId),
-			]
-		);
+		parent::__construct('messageReceived', ['id' => $messageId]);
+	}
+
+	public function init() {
 		$this->addClass('d-inline-block');
+
+		$this->setOptions([
+			'button-attributes' => ['class' => 'btn-sm'],
+			'confirm' => 'Are you sure to delete this message?',
+			'flash-message' => 'Message deleted successfully',
+			'selector' => '#message-' . $this->post('id', $this->messageId),
+		]);
 	}
 
 }
